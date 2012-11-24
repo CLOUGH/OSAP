1fd8f029f8ab1c2b561ffe97b00ae729feacb35e
shane campbell <shanec132006@hotmail.com>
2012-11-24 08:07:20 -0500

    Update the Course Registration
    Stored the courses as arrays to push to the db

--- view_recommended_courses/list_course/controller/register.php
+++ view_recommended_courses/list_course/controller/register.php
@@ -1,13 +1,52 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html>
+<head>
 
+
+<?php
+include_once '../../../common/php_class/user.php';
+foreach (glob('../../../common/php_controller/*.php') as $filename)
+include_once $filename;
+
+	
+session_start();
+$user = $_SESSION['user'];
+	$subject = array() ;
+
+	foreach($_POST['course']  as  $payamt)  {
+		 array_push($subject, $payamt);
+	}
+
+?>
+	<meta charset="utf-8"/>
+	<title>Home</title>
+	<link rel ="stylesheet" type="text/css" href="../../../common/stylesheet/
+	main_style.css" />
+	<link rel ="stylesheet" type="text/css" href="../../../common/stylesheet/navigation_bar.css" />
+	<script src="../../../common/jQuery/jquery-1.8.2.js"></script>
+	<script type="text/javascript" src="stylesheet/header.js"></script>
+
+</head>
+<body>
+	<div class="wrapper">
+			<div class="header" >
+				<?php echo get_notification_bar($user->getFirstName(),$user->getLastName()); ?>
+				<h1 id="test">Student Online Advisory Portal</h1>
+				<?php echo get_navigation_bar($user->getType()); ?>
+			</div>
+			<div class="content" >
+
+<?php
+
+		
 
 /*--------------------------------------------------/
 /			Function to Register for Courses        /
 /--------------------------------------------------*/
 function register($subject, $session_name){
 
-
+$person = $session_name->getFirstName();
+$serializeArray =serialize($subject); 
 /*--------------------------------------------------/
 /				Connects to Database                /
 /--------------------------------------------------*/
@@ -20,10 +59,15 @@ if (mysqli_connect_errno()) {
 /*--------------------------------------------------/
 /				Updates Course Info                 /
 /--------------------------------------------------*/
-$sql = "UPDATE registered_courses SET Courses = '$subject' WHERE Id_Number='$session_name'"; 
+$sql = "UPDATE registered_courses SET Courses = '$serializeArray' WHERE user_name='$person'"; 
 
 if ($conn->query($sql) === TRUE) {
-  echo 'Successfully Registered For Course:'.' '.$subject;
+  echo 'Successfully Registered For Course: <br />';
+  for ($i=0; $i < count($subject) ; $i++) { 
+  	echo $subject[$i].'<br/>';
+  }
+
+
 }
 else {
  echo 'Error: '. $conn->error;
@@ -34,12 +78,20 @@ $conn->close();
 
 
 }
-foreach($_POST['course']  as  $value)  {
-		$subject.= "$value\n";
-	}
-$session_name = $_SESSION['username'];
 
-register($subject, $session_name);
 
-var_dump($subject);
+register($subject, $user);
+
+
+
 ?>
+		</div>
+
+		<div class="footer">
+			<a href="">Home</a>
+		</div>
+	</div>
+
+
+</body>
+</html>
